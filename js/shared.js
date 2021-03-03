a7f0ef7cc62d29f5ac44eb86498376375f9101dc
M Somerville <matthew-github@dracos.co.uk>
2021-03-03 17:57:21 +0000

    Activate remember boxes, skip remembered pages.

--- js/shared.js
+++ js/shared.js
@@ -14,3 +14,57 @@ $('[data-form-action]').on('change', function() {
   }
 
 });
+
+$('form').on('submit', function() {
+    var $form = $(this);
+    var next = $form.attr('action');
+    var remembered = JSON.parse(localStorage.getItem('remembered') || '{}');
+    var box = document.getElementById('remember');
+    if (box) {
+        var current = location.pathname;
+        if (box.checked) {
+            remembered[current] = {
+                'answers': $form.serializeArray(),
+                'next': next,
+            };
+        } else {
+            delete remembered[current];
+        }
+        localStorage.setItem('remembered', JSON.stringify(remembered));
+    }
+    var action = skipPage(next, remembered);
+    if (action) {
+        $form.attr('action', action);
+    }
+});
+
+// Initial start might cause a skip too
+$('#start-button').on('click', function(e) {
+    var remembered = JSON.parse(localStorage.getItem('remembered') || '{}');
+    var action = skipPage('/report-a-fault/location-id/', remembered);
+    if (action) {
+        updateState('id-known', 'yes');
+        e.preventDefault();
+        location.href = action;
+        return;
+    }
+    action = skipPage('/report-a-fault/location-site/', remembered);
+    if (action) {
+        updateState('id-known', 'no');
+        e.preventDefault();
+        location.href = action;
+        return;
+    }
+});
+
+function skipPage(page, remembered) {
+    var newPage;
+    while (remembered[page]) {
+        remembered[page]['answers'].forEach(function(a) {
+            updateState(a.name, a.value);
+        });
+        page = remembered[page]['next'];
+        newPage = page;
+    }
+    return newPage;
+}
